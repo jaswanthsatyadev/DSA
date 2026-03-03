5c65fe0561a88b701442b3cf03d0bcf7c1b169af
Jaswanth Satya Dev <jaswanthsatyadev55@gmail.com>
2026-03-03 17:54:17 +0530

    ShipIt: two-sum

---
+++ leetcode/two-sum/two-sum.cpp
@@ -0,0 +1,27 @@
+// ShipIt
+// Problem:
+// URL: https://leetcode.com/problems/two-sum/description/
+// Language: cpp
+// Pushed: 2026-03-03T12:24:16.678Z
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp;
+
+        for (int i = 0; i < nums.size(); i++) {
+            int needed = target - nums[i];
+
+            if (mp.find(needed) != mp.end()) {
+                return {mp[needed], i};
+            }
+
+            mp[nums[i]] = i;
+        }
+
+        return {};
+    }
+};
